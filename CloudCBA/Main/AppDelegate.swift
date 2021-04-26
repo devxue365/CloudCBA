cf771d39228eb8b3556bd6846a5f3da5f9c7309c
devxue <devxue@aliyun.com>
2021-04-26 22:27:09 +0800

    ref |> 添加日志初始化，日志本地保存；修改.gitignore，添加.DS_Store文件忽略。

--- CloudCBA/Main/AppDelegate.swift
+++ CloudCBA/Main/AppDelegate.swift
@@ -6,11 +6,10 @@
 //
 
 import UIKit
+import XCGLogger
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -18,6 +17,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // 设置编译模式
         DXHttp.shared.buildConfig = .release
         
+        // 日志
+        let log = XCGLogger.default
+        // 日志文件输出地址
+        let cachePath = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+        let logURL = cachePath.appendingPathComponent("log.txt")
+        // 日志设置
+        log.setup(level: .debug,
+                  showThreadName: true,
+                  showLevel: true,
+                  showFileNames: true,
+                  showLineNumbers: true,
+                  writeToFile: logURL,
+                  fileLevel: .debug)
+        
+/*
+log.verbose("一条verbose消息：程序执行时最详细的信息。")
+log.debug("一条debug消息：用于代码调试。")
+log.info("一条info消息：常用与用户在console.app中查看。")
+log.warning("一条warning消息：警告消息，表示一个可能的错误。")
+log.error("一条error消息：表示产生了一个可恢复的错误，用于告知发生了什么事情。")
+log.severe("一条severeError消息：表示产生了一个严重错误。程序可能很快会奔溃。")
+*/
+
         return true
     }
 
